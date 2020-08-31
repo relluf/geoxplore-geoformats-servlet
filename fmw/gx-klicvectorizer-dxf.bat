7d72724b29d41eb27e6ff19ccce9f6c0ba7d67dc
Ralph Kazemier <ralph.kazemier@gmail.com>
2020-08-30 22:32:15 -0500

    1st commit

--- fmw/gx-klicvectorizer-dxf.bat
+++ fmw/gx-klicvectorizer-dxf.bat
@@ -4,5 +4,5 @@ date /t >> D:\apps\GeoFormats\fmw\gx-klicvectorizer.log
 time /t >> D:\apps\GeoFormats\fmw\gx-klicvectorizer.log
 echo IN: %1 >> D:\apps\GeoFormats\fmw\gx-klicvectorizer.log
 echo OUT: %2 >> D:\apps\GeoFormats\fmw\gx-klicvectorizer.log
-REM TODO I think the -DXF_DEST_FILE is ignore and hard-coded to layers.dxf 
-"C:\Program Files\FME\fme.exe" D:\apps\GeoFormats\KV.fmw --GML_INPUT_FILE %1  --GML_INPUT_FILE_3 %1 --DGN_DEST_DIR %2 --DXF_DEST_DIR %2 --DXF_DEST_FILE %2\LG_nl-imkl.dxf >> D:\apps\GeoFormats\fmw\gx-klicvectorizer.log 2>&1
+echo FORMAT: SHAPE
+"C:\Program Files\FME\fme.exe" D:\apps\GeoFormats\KV-dxf.fmw --GML_INPUT_FILE %1  --GML_INPUT_FILE_3 %1 --DestDataset_SHAPEFILE %2 >> D:\apps\GeoFormats\fmw\gx-klicvectorizer.log 2>&1
